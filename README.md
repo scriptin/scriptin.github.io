f355b75c6bc31c08b7dd6bd6d87ef6a812648b16
Dmitry Shpika <scrptn@gmail.com>
2024-07-02 19:00:36 +0200

    Add script to watch and regenrate Less styles on change; Use content hash for CSS files; Import CSS files dynamically

--- README.md
+++ README.md
@@ -10,7 +10,4 @@ Then go to `http://localhost:4000`. Port `35729` is used for live reloading.
 ## Refreshing CSS
 
 1. Install [Devbox](https://www.jetpack.io/devbox/)
-2. Run `devbox run styles:watch` (or `devbox run styles:build` for production)
-
-Note that CSS files may be cached, so during development it is recommended to keep DevTools open
-with the 'Disable cache' checkbox set oin the Network tab.
+2. Run `devbox run styles:watch` - this will also rebuild the existing styles if they've changed
--- watch-styles.js
+++ watch-styles.js
@@ -1,16 +1,79 @@
-import { watch } from 'node:fs';
-import { execSync } from 'node:child_process';
+import { watch, readFileSync, writeFileSync, readdirSync, unlinkSync } from 'node:fs';
+import path from 'node:path';
+import { createHash } from 'node:crypto';
+import less from 'less';
+import PluginCleanCSS from 'less-plugin-clean-css';
 
-watch('./less', (eventType, filename) => {
-    if (!filename) return;
-    const isTemporary = filename.endsWith('~');
-    const isStyle = filename.endsWith('.less') || filename.endsWith('.less');
-    if (isTemporary || !isStyle) return;
+const SOURCE_PATH = './less';
+const ROOT_FILE_PATH = `${SOURCE_PATH}/style.less`;
+const OUTPUT_DIR = './assets/css';
 
-    if (eventType === 'change') {
-        console.log('Rebuilding styles...');
-        execSync('lessc less/style.less --clean-css assets/css/style.min.css');
-    } else {
-        console.log(`Ignored event type ${eventType} on file ${filename}`);
+function replaceRelativeImports(fileContents, filePath) {
+    const pathPrefixParts = filePath.split('/')
+    pathPrefixParts.pop();
+    const pathPrefix = pathPrefixParts.join('/');
+    return fileContents
+        .replaceAll(/@import \(inline\) "(.*)";/g, `@import (inline) "./${pathPrefix}/$1";`)
+        .replaceAll(/@import "(.*)";/g, `@import "./${pathPrefix}/$1";`);
+}
+
+async function renderCss(filePath) {
+    const fileContents = readFileSync(filePath, { encoding: 'utf8' });
+    const css = replaceRelativeImports(fileContents, filePath);
+    const cleanCSSPlugin = new PluginCleanCSS({ advanced: true });
+    return less.render(css, { env: 'production', plugins: [cleanCSSPlugin] });
+}
+
+function getContentHash(s) {
+    return createHash('md5').update(s).digest("hex")
+}
+
+function removeAllFilesSync(dir, match) {
+    const files = readdirSync(dir);
+    for (const file of files) {
+        if (!match(file)) continue;
+        const filePath = path.join(dir, file);
+        unlinkSync(filePath);
     }
-});
+}
+
+function isStyleFile(fileName) {
+    if (!fileName) return;
+    const isTemporary = fileName.endsWith('~');
+    const isStyle = fileName.endsWith('.less') || fileName.endsWith('.css');
+    return !isTemporary && isStyle;
+}
+
+function rebuildStyles(rootFilePath, outputDir) {
+    renderCss(rootFilePath).then(({ css }) => {
+        const extToDelete = '.min.css';
+        console.log(`Removing previously generated files (*${extToDelete}) in ${outputDir}`);
+        removeAllFilesSync(outputDir, name => name.endsWith(extToDelete));
+        const fileBaseName = rootFilePath.split('/').pop().split('.').shift();
+        const hash = getContentHash(css).substring(0, 8);
+        const newFilePath = `${outputDir}/${fileBaseName}-${hash}.min.css`;
+        console.log('Writing to ', newFilePath);
+        writeFileSync(newFilePath, css, { encoding: 'utf8' });
+    });
+}
+
+function watchLessDirectory(path, callback) {
+    watch(path, (eventType, fileName) => {
+        if (!isStyleFile(fileName)) return;
+
+        if (eventType === 'change') {
+            console.log('Rebuilding styles...');
+            callback();
+        } else {
+            console.log(`Ignored event type ${eventType} on file ${fileName}`);
+        }
+    });
+}
+
+function rebuildCallback() {
+    rebuildStyles(ROOT_FILE_PATH, OUTPUT_DIR);
+}
+
+// Rebuild when starting, just in case this script wasn't running when files were edited
+rebuildCallback();
+watchLessDirectory(SOURCE_PATH, rebuildCallback);
